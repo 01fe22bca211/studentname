d3a9f6ee664d26b7a4df22955496c685098e064c
Vaishali <01fe22bca211@kletech.ac.in>
2024-03-18 19:22:33 +0530

    student information

---
+++ Student.java
@@ -0,0 +1,30 @@
+public class Student {
+
+    String name="kiran";
+    int age=19;
+    String month;
+    int date;
+    int year;
+
+    public Student(int date, String month, int year) {
+        this.date=date;
+        this.month=month;
+        this.year=year;
+    }
+
+    public void display(){
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+    }
+
+    public void read(){
+        System.out.print("Date of birth of the Student is ");
+        System.out.print(date +"/" + month+"/"+year);
+    }
+
+    public static void main(String[] args){
+        Student s = new Student(16, "August", 2004);
+        s.display();
+        s.read();
+    }
+}
